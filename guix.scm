ce6f0a3e85fc1d66fa2e94eb0bb904fa53942520
pukkamustard <pukkamustard@posteo.net>
2021-02-03 10:53:01 +0100

    Base32: encode_*

---
+++ guix.scm
@@ -0,0 +1,26 @@
+(use-modules
+ (guix packages)
+ (guix download)
+ (guix git-download)
+ (guix build-system dune)
+ ((guix licenses) #:prefix license:)
+ (gnu packages ocaml)
+ (gnu packages libevent))
+
+(define-public ocaml-base32
+  (package
+    (name "ocaml-base32")
+    (version "0.0.0")
+    (source #f)
+    (build-system dune-build-system)
+    (arguments '())
+    (native-inputs
+     `(("merlin" ,ocaml-merlin)
+       ("dot-merlin-reader" ,ocaml-dot-merlin-reader)
+       ("ocp-indent" ,ocaml-ocp-indent)))
+    (home-page "https://gitlab.com/public.dream/DROMEDAR/ocaml-base32")
+    (synopsis "Cross-platform cryptographic primitives required for DROMEDAR")
+    (description "sdfsdf")
+    (license license:agpl3+)))
+
+ocaml-base32
