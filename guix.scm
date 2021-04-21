c22ab5e65d0e619017960c8771a3b85f0b8030d5
pukkamustard <pukkamustard@posteo.net>
2021-04-21 15:36:13 +0200

    guix.scm: Remove unused native-inputs

--- guix.scm
+++ guix.scm
@@ -15,10 +15,7 @@
     (build-system dune-build-system)
     (arguments '())
     (native-inputs
-     `(("merlin" ,ocaml-merlin)
-       ("dot-merlin-reader" ,ocaml-dot-merlin-reader)
-       ("ocp-indent" ,ocaml-ocp-indent)
-       ("alcotest" ,ocaml-alcotest)
+     `(("alcotest" ,ocaml-alcotest)
        ("qcheck" ,ocaml-qcheck)))
     (home-page "https://gitlab.com/public.dream/DROMEDAR/ocaml-base32")
     (synopsis "Cross-platform cryptographic primitives required for DROMEDAR")
